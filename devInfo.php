f7a0e16b01e41837482cb8f514f0b35e2885d87c
nothazel24 <frdnryann@gmail.com>
2024-10-29 12:01:19 +0700

    re-design layout devInfo.php

--- devInfo.php
+++ devInfo.php
@@ -3,21 +3,18 @@
     <?php include 'others/head.php'; ?>
     <div class="container p-5 my-5">
         <h1 class="header">Dev's Profile'</h1>
+
         <section class="devOne">
-            <div class="line mb-5"></div>
 
             <div class="cardBg">
-
                 <div class="section1">
-                    <img src="assets/silder.jpg" alt="dev pfp">
-                    <h5>Contact</h5>
-                    <div class="socialMedList">
-                        <a href="#"><img src="/assets/guthib.svg"></a>
-                        <a href="#"><img src="/assets/racistapp.svg"></a>
+                    <div class="bannerBg">
+                        <h1 class="bannerHead"><?= "Username"; ?></h1>
                     </div>
                 </div>
 
                 <div class="section2">
+                    <img src="assets/scarybear.png" alt="devFurry" class="devPfp" width="80px" height="80px">
                     <h3>Dev Name</h3>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint hic maxime, doloribus porro
                         repellat temporibus consequuntur quod minus cumque, sequi deleniti corporis inventore adipisci
@@ -33,6 +30,7 @@
                             <img src="assets/scarybear.png" alt="#" class="imgStack">
                         </div>
                     </div> <!-- SECTION 3 -->
+
                     <div class="favMusic">
                         <img src="assets/epic.jpg" alt="epic" class="img">
                         <div class="textArea">
@@ -48,27 +46,20 @@
         <section class="devTwo">
 
             <h3 class="sub-header">Projects and Others</h3>
-            <div class="line mb-5"></div>
             <div class="extrasWrapper">
 
                 <?php for ($k = 0; $k <= 1; $k++): ?>
-                    <div class="extras<?= $k ?>">
-                        <div class="extrasCard">
+                    <div class="extrasCardContent">
+                        <img src="assets/silder.jpg" class="project-photo" alt="projectPhoto" width="80px" height="80px">
+                        <div class="extrasCardContent2">
+                            <h3>Project Name</h3>
+                            <p>project desc</p>
+                            <a href="#" class="projectLink" target="_self">
+                                <p>View Projects</p>
+                            </a>
+                        </div> <!-- EXTRAS CARD CONTENT 2-->
 
-                            <div class="extrasCardContent">
-                                <img src="" alt="">
-                                <div class="extrasCardContent2">
-                                    <h3>Project Name</h3>
-                                    <p>project desc</p>
-                                    <div class="view">
-                                        <a href="#" target="_self">
-                                            <p>View Projects</p>
-                                        </a>
-                                    </div>
-                                </div> <!-- VIEW -->
-                            </div> <!-- EXTRAS CARD CONTENT -->
-                        </div> <!-- EXTRAS CARD -->
-                    </div> <!-- EXTRAS ONE -->
+                    </div> <!-- EXTRAS CARD CONTENT -->
                 <?php endfor; ?>
 
             </div> <!-- EXTRAS WRAPPER -->
